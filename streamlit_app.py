4ea58eabdaed47efc4c5fe42a491dcfc71683a69
pduran24 <pabloduranp5@gmail.com>
2025-07-24 15:30:09 +0200

    Correccion f xd

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from oraculo.modelo import ModeloOraculo
 
 
 st.image ("https://imgproxy.attic.sh/insecure/f:png/plain/https://attic.sh/d7soo271ta5vqrjszhp9tfn30y89")
-st.title(" SAVE TEQUILA")
+st.title(" SAFE TEQUILA")
 st.markdown("Consultar el estado de Tequila y su probabilidad de estar perdida")
 
 
